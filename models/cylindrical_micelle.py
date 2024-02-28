remove volume normalization for sasfit comparision; fix integration issues; much better compariison with sasfit

models/cylindrical_micelle.py
@@ -73,7 +73,7 @@ def Iq(q,
 
     beta_core = v_core * (rho_core - rho_solv)
     beta_corona = v_corona * (rho_corona - rho_solv)
-    alpha = np.linspace(0, np.pi, num=100)
+    alpha = np.linspace(0, 0.5*np.pi, num=5000)
 
     # Self-correlation term of the core
     bes_core = psi(q, radius_core, length_core, alpha)
@@ -105,7 +105,7 @@ def Iq(q,
     i_micelle = term1 + term2 + term3 + term4 
 
     # Normalize intensity by total volume
-    return i_micelle/v_total
+    return i_micelle/1.0
 
 Iq.vectorized = True  # Iq does not accept an array of q values
 

models/spherical_micelle.py
@@ -37,7 +37,7 @@ def Iq(q,
         n_aggreg=67,
         x_solv = 0.0
         ):
-    radius_core = np.power((n_aggreg*v_core)/(1-x_solv)*(3/(4*np.pi)), 1/3)
+    n_aggreg = ((1-x_solv)*(4/3)*np.pi*(radius_core**3))/v_core
     v_total = n_aggreg*(v_core+v_corona)
     rho_solv = sld_solvent     # sld of solvent [1/A^2]
     rho_core = sld_core        # sld of core [1/A^2]
@@ -45,6 +45,7 @@ def Iq(q,
 
     beta_core = v_core * (rho_core - rho_solv)
     beta_corona = v_corona * (rho_corona - rho_solv)
+    beta_total = (beta_core+beta_corona)*n_aggreg
 
     # Self-correlation term of the core
     bes_core = sas_3j1x_x(q*radius_core)
@@ -68,7 +69,7 @@ def Iq(q,
     # I(q)_micelle : Sum of 4 terms computed above
     i_micelle = term1 + term2 + term3 + term4
 
-    return i_micelle/v_total
+    return i_micelle/(1.0)
 
 Iq.vectorized = True  # Iq accepts an array of q values
 